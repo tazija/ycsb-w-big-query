4644b8c90d6c8e01ea8c5d7603bee9f3301de967
Mikalai Lushchytski <mikalai.lushchytski@altoros.com>
2018-06-12 21:29:32 +0300

    added instances count support which affects number of threads election to run

--- multithreaded_runner.sh
+++ multithreaded_runner.sh
@@ -3,16 +3,17 @@
 # Print the usage message
 function printHelp () {
   echo "Usage: "
-  echo "  ./multithreaded_runner.sh -w <workload_name> -d <database_name> -p arg1=argvalue1 -p arg2=argvalue2"
+  echo "  ./multithreaded_runner.sh -w <workload_name> -d <database_name> -i <client_instances_count> -p arg1=argvalue1 -p arg2=argvalue2"
   echo "  ./multithreaded_runner.sh -h|--help (print this message)"
   echo "    -w <workload> - workload name to launch"
   echo "    -d <workload> - database use. Examples: 'couchbase2' | 'cassandra-cql' | ..."
+  echo "    -i <client_instances_count> - client instance count. currently supported values are 1, 2, 3;"
   echo "    -p <ycsb_parameter> - a set of ycsb extra parameters to add. Should be in format '-p name=value'"
 }
 
 # Parse commandline args
 ARGS=()
-while getopts "h?w:d:p:" opt; do
+while getopts "h?w:d:i:p:" opt; do
   case "$opt" in
     h|\?)
       printHelp
@@ -21,6 +22,8 @@ while getopts "h?w:d:p:" opt; do
     w)  WORKLOAD=$OPTARG    # workload name
     ;;
     d)  DB=$OPTARG
+    ;;
+    i)  INSTANCES=$OPTARG
     ;;
     p)  ARGS+=("-p $OPTARG")
     ;;
@@ -37,10 +40,21 @@ if [ -z "${DB}" ]; then
     exit 1;
 fi
 
-for threads in 25 50 75 100 125 150 175 200 250 300 400 500; do
+if [ "${INSTANCES}" -eq "1" ]; then
+    THREADS=(25 50 75 100 125 150 175 200 250 300 400 500)
+elif [ "${INSTANCES}" -eq "2" ]; then
+    THREADS=(25 50 75 100 125 150 200 250 300)
+elif [ "${INSTANCES}" -eq "3" ]; then
+    THREADS=(15 30 50 75 100 125 150 200)
+else
+    echo "Instances number is either not specified or not valid (valid values are 1, 2, 3). Aborting evaluation"
+    exit 1;
+fi
+
+for threads in ${THREADS[@]}; do
  echo " >> Launching workload ${WORKLOAD} in ${threads} threads..."
 
- echo "mkdir -p ./results/4-nodes/couchbase/${WORKLOAD} && ./bin/ycsb run -s ${DB} -P workloads/${WORKLOAD} -threads ${threads} -p statsexportfile=./results/4-nodes/couchbase/${WORKLOAD}/results.csv ${ARGS[@]}" | sh
+ echo "mkdir -p ./results/4-nodes/couchbase/${WORKLOAD} && ./bin/ycsb run -s ${DB} -P workloads/${WORKLOAD} -threads ${threads} -p statsexportfile=./results/4-nodes/couchbase/${WORKLOAD}/results.csv ${ARGS[@]}" #| sh
 
  echo " << Finished workload ${WORKLOAD} execution in ${threads} threads!"
 done
