571bb2803ad443079f33347d9ec1e233179cee17
Mikalai Lushchytski <mikalai.lushchytski@altoros.com>
2018-06-13 16:02:53 +0300

    added support for output file customization, 6 and 7 clients

--- multithreaded_runner.sh
+++ multithreaded_runner.sh
@@ -3,17 +3,19 @@
 # Print the usage message
 function printHelp () {
   echo "Usage: "
-  echo "  ./multithreaded_runner.sh -w <workload_name> -d <database_name> -i <client_instances_count> -p arg1=argvalue1 -p arg2=argvalue2"
+  echo "  ./multithreaded_runner.sh -w <workload_name> -d <database_name> -i <client_instances_count> -r <result_file_name> -p arg1=argvalue1 -p arg2=argvalue2"
   echo "  ./multithreaded_runner.sh -h|--help (print this message)"
   echo "    -w <workload> - workload name to launch"
   echo "    -d <workload> - database use. Examples: 'couchbase2' | 'cassandra-cql' | ..."
-  echo "    -i <client_instances_count> - client instance count. currently supported values are 1, 2, 3, 4, 5;"
+  echo "    -i <client_instances_count> - client instance count. currently supported values are 1, 2, 3, 4, 5, 6, 7;"
+  echo "    -r <result_file_name> - [Optional] result file name. If not specified, defaults to 'results'"
   echo "    -p <ycsb_parameter> - a set of ycsb extra parameters to add. Should be in format '-p name=value'"
 }
 
 # Parse commandline args
 ARGS=()
-while getopts "h?w:d:i:p:" opt; do
+RESULT_FILE="results"
+while getopts "h?w:d:i:r:p:" opt; do
   case "$opt" in
     h|\?)
       printHelp
@@ -24,6 +26,8 @@ while getopts "h?w:d:i:p:" opt; do
     d)  DB=$OPTARG
     ;;
     i)  INSTANCES=$OPTARG
+    ;;
+    r)  RESULT_FILE=$OPTARG
     ;;
     p)  ARGS+=("-p $OPTARG")
     ;;
@@ -50,15 +54,19 @@ elif [ "${INSTANCES}" -eq "4" ]; then
     THREADS=(10 25 35 50 75 100 125 150 175)
 elif [ "${INSTANCES}" -eq "5" ]; then
     THREADS=(10 20 30 40 50 60 70 80 100 120 140)
+elif [ "${INSTANCES}" -eq "6" ]; then
+    THREADS=(10 20 30 40 50 60 70 80 100 120)
+elif [ "${INSTANCES}" -eq "7" ]; then
+    THREADS=(5 10 15 20 25 30 40 50 60 70 80 100)
 else
-    echo "Instances number is either not specified or not valid (valid values are 1, 2, 3, 4, 5). Aborting evaluation"
+    echo "Instances number is either not specified or not valid (valid values are 1, 2, 3, 4, 5, 6, 7). Aborting evaluation"
     exit 1;
 fi
 
 for threads in ${THREADS[@]}; do
  echo " >> Launching workload ${WORKLOAD} in ${threads} threads..."
 
- echo "mkdir -p ./results/4-nodes/${DB}/${WORKLOAD} && ./bin/ycsb run -s ${DB} -P workloads/${WORKLOAD} -threads ${threads} -p statsexportfile=./results/4-nodes/${DB}/${WORKLOAD}/results.csv ${ARGS[@]}" | sh
+ echo "mkdir -p ./results/4-nodes/${INSTANCES}-clients/${DB}/${WORKLOAD} && ./bin/ycsb run -s ${DB} -P workloads/${WORKLOAD} -threads ${threads} -p statsexportfile=./results/4-nodes/${INSTANCES}-clients/${DB}/${WORKLOAD}/${RESULT_FILE}.csv ${ARGS[@]}" | sh
 
  echo " << Finished workload ${WORKLOAD} execution in ${threads} threads!"
 done
